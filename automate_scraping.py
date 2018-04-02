6982446f7609484708e859dc8b7089d35b3ea5c9
Elliot <elliot.james.williams@gmail.com>
2018-04-02 00:37:51 -0400

    Refactored code to allow for automated scraping in two month increments

--- automate_scraping.py
+++ automate_scraping.py
@@ -1,4 +1,41 @@
-import get_metadata as meta
 import scrape as scrape
+import metadata as meta
+import os
+import datetime
 
-print("hi")
+# edit these three variables
+user = 'nytimes' # Twitter account to scrape
+years = [2006 + x for x in range(13)] # years to scrape
+
+def getTwoMonthIntervals(year):
+    if year % 4 == 0:
+        two_month_intervals = [
+        (datetime.datetime(year, 1,  1), datetime.datetime(year, 2,  29)),
+        (datetime.datetime(year, 3,  1), datetime.datetime(year, 4,  30)),
+        (datetime.datetime(year, 5,  1), datetime.datetime(year, 6,  30)),
+        (datetime.datetime(year, 7,  1), datetime.datetime(year, 8,  31)),
+        (datetime.datetime(year, 9,  1), datetime.datetime(year, 10, 31)),
+        (datetime.datetime(year, 11, 1), datetime.datetime(year, 12, 31))]
+    else:
+        two_month_intervals = [
+        (datetime.datetime(year, 1,  1), datetime.datetime(year, 2,  28)),
+        (datetime.datetime(year, 3,  1), datetime.datetime(year, 4,  30)),
+        (datetime.datetime(year, 5,  1), datetime.datetime(year, 6,  30)),
+        (datetime.datetime(year, 7,  1), datetime.datetime(year, 8,  31)),
+        (datetime.datetime(year, 9,  1), datetime.datetime(year, 10, 31)),
+        (datetime.datetime(year, 11, 1), datetime.datetime(year, 12, 31))]
+    return two_month_intervals
+
+if __name__ == "__main__":
+    for year in years:
+        i=0
+        intervals = getTwoMonthIntervals(year)
+        for interval in intervals:
+            if not os.path.isfile("./data/{}_{}_{}.csv".format(user,i, year)):
+                start, end = interval
+                scrape.run(user, start, end)
+                meta.run(user, "{}_{}".format(i, year))
+                os.remove('./all_ids.json')
+            else:
+                print(user+"_{}_{}.csv already exists".format(i, year))
+            i += 1
--- scrape.py
+++ scrape.py
@@ -18,7 +18,7 @@ def format_day(date):
     year = str(date.year)
     return '-'.join([year, month, day])
 
-def form_url(since, until):
+def form_url(user, since, until):
     p1 = 'https://twitter.com/search?f=tweets&vertical=default&q=from%3A'
     p2 =  user + '%20since%3A' + since + '%20until%3A' + until + 'include%3Aretweets&src=typd'
     return p1 + p2
@@ -28,7 +28,7 @@ def increment_day(date, i):
 
 def run(user, start, end):
     # only edit these if you're having problems
-    delay = 1  # time to wait on each page load before reading the page
+    delay = 0.7  # time to wait on each page load before reading the page
     driver = webdriver.Chrome()  # options are Chrome() Firefox() Safari()
 
     # don't mess with this stuff
@@ -40,55 +40,62 @@ def run(user, start, end):
     ids = []
 
     for day in range(days):
-    d1 = format_day(increment_day(start, 0))
-    d2 = format_day(increment_day(start, 1))
-    url = form_url(d1, d2)
-    print(url)
-    print(d1)
-    driver.get(url)
-    sleep(delay)
+        d1 = format_day(increment_day(start, 0))
+        d2 = format_day(increment_day(start, 1))
+        url = form_url(user, d1, d2)
+        print(url)
+        print(d1)
+        try:
+            driver.get(url)
+        except:
+            driver.close()
+            driver = webdriver.Chrome()
+            driver.get(url)
+            print("HIT EXCEPTION -- if something happens wrong now you know why")
 
-    try:
-        found_tweets = driver.find_elements_by_css_selector(tweet_selector)
-        increment = 10
+        sleep(delay)
 
-        while len(found_tweets) >= increment:
-            print('scrolling down to load more tweets')
-            driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-            sleep(delay)
+        try:
             found_tweets = driver.find_elements_by_css_selector(tweet_selector)
-            increment += 10
+            increment = 10
 
-        print('{} tweets found, {} total'.format(len(found_tweets), len(ids)))
+            while len(found_tweets) >= increment:
+                print('scrolling down to load more tweets')
+                driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+                sleep(delay)
+                found_tweets = driver.find_elements_by_css_selector(tweet_selector)
+                increment += 10
 
-        for tweet in found_tweets:
-            try:
-                id = tweet.find_element_by_css_selector(id_selector).get_attribute('href').split('/')[-1]
-                ids.append(id)
-            except StaleElementReferenceException as e:
-                print('lost element reference', tweet)
+            print('{} tweets found, {} total'.format(len(found_tweets), len(ids)))
 
-    except NoSuchElementException:
-        print('no tweets on this day')
+            for tweet in found_tweets:
+                try:
+                    id = tweet.find_element_by_css_selector(id_selector).get_attribute('href').split('/')[-1]
+                    ids.append(id)
+                except StaleElementReferenceException as e:
+                    print('lost element reference', tweet)
 
-    start = increment_day(start, 1)
+        except NoSuchElementException:
+            print('no tweets on this day')
+
+        start = increment_day(start, 1)
 
 
     try:
-    with open(twitter_ids_filename) as f:
-        all_ids = ids + json.load(f)
-        data_to_write = list(set(all_ids))
-        print('tweets found on this scrape: ', len(ids))
-        print('total tweet count: ', len(data_to_write))
+        with open(twitter_ids_filename) as f:
+            all_ids = ids + json.load(f)
+            data_to_write = list(set(all_ids))
+            print('tweets found on this scrape: ', len(ids))
+            print('total tweet count: ', len(data_to_write))
     except FileNotFoundError:
-    with open(twitter_ids_filename, 'w') as f:
-        all_ids = ids
-        data_to_write = list(set(all_ids))
-        print('tweets found on this scrape: ', len(ids))
-        print('total tweet count: ', len(data_to_write))
+        with open(twitter_ids_filename, 'w') as f:
+            all_ids = ids
+            data_to_write = list(set(all_ids))
+            print('tweets found on this scrape: ', len(ids))
+            print('total tweet count: ', len(data_to_write))
 
     with open(twitter_ids_filename, 'w') as outfile:
-    json.dump(data_to_write, outfile)
+        json.dump(data_to_write, outfile)
 
     print('all done here')
     driver.close()
