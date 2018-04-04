24a9c0f0e4b237f9af89589805ea09b117377a0a
Elliot <elliot.james.williams@gmail.com>
2018-04-04 15:54:10 -0400

    Updated scrape.py to be extendable for unreliable browsers / scraping (ie I think Twitter marked my browser so it doesn't load completely, needs new window)

--- scrape.py
+++ scrape.py
@@ -8,6 +8,7 @@ It is only slightly modified
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, NoSuchWindowException
+from selenium.webdriver.firefox.options import Options
 from time import sleep
 import json
 import datetime
@@ -23,6 +24,7 @@ def form_url(user, since, until):
     p2 =  user + '%20since%3A' + since + '%20until%3A' + until + 'include%3Aretweets&src=typd'
     return p1 + p2
 
+
 def increment_day(date, i):
     return date + datetime.timedelta(days=i)
 
@@ -34,16 +36,17 @@ def getURL(driver, url, num_errors=0):
             print("There were 10 NoSuchWindowExceptions for one page...")
             raise e
         num_errors += 1
-        driver = webdriver.Chrome()
-        getURL(driver, url, num_errors)
+        driver = webdriver.Firefox()
+        driver = getURL(driver, url, num_errors)
         # Creates a new driver, because the connection to this one has beene lost
 
         driver.get(url)
+    return driver
 
 def run(user, start, end):
     # only edit these if you're having problems
-    delay = 0.7  # time to wait on each page load before reading the page
-    driver = webdriver.Chrome()  # options are Chrome() Firefox() Safari()
+    delay = 1.5  # time to wait on each page load before reading the page
+    driver = webdriver.Firefox()  # options are Chrome() Firefox() Safari()
 
     # don't mess with this stuff
     twitter_ids_filename = '{}_all_ids.json'.format(user)
@@ -59,13 +62,15 @@ def run(user, start, end):
         url = form_url(user, d1, d2)
         print(url)
         print(d1)
-        getURL(driver, url)
+        driver = getURL(driver, url)
         sleep(delay)
 
         try:
             found_tweets = driver.find_elements_by_css_selector(tweet_selector)
             increment = 10
 
+            # If the driver is lost track of somewhere in here,
+            # we want the process to fail
             while len(found_tweets) >= increment:
                 print('scrolling down to load more tweets')
                 driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
